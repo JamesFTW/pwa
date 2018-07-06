50a76602f731ee8cc51b8c346204e57c800d4cf4
James Andrews <bdaddyja@gmail.com>
2018-07-06 14:42:58 -0700

    Some promise stuff.

---
+++ promises.js
@@ -0,0 +1,35 @@
+function addExtra(price) {
+
+  return new Promise(function(resolve, reject) {
+    console.log(price)
+    if (price > 2) reject("Price cannont exceed 3")
+
+     setTimeout(function() {
+       resolve(price + 1)
+     }, 300)
+  })
+ }
+
+const price = new Promise(function(resolve, reject) {
+  setTimeout(() => {
+    resolve("Price A")
+  }, 300)
+})
+
+const slowPrice = new Promise(function(resolve, reject) {
+  setTimeout(() => {
+    resolve("Price B")
+  }, 800)
+})
+
+const promises = [price, slowPrice]
+
+//returns an array of all resolved values.
+Promise.all(promises).then((resolvedPromises) => {
+  console.log(resolvedPromises)
+})
+
+//returns first resolved value
+Promise.race(promises).then((firstDone) => {
+  console.log(firstDone)
+})
