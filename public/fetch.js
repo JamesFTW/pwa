6cd2f611703b2a4c153808ea96a5dfac07b11544
James Andrews <bdaddyja@gmail.com>
2018-08-13 00:28:00 -0700

    remove a lot of stuff from fetch

--- public/fetch.js
+++ public/fetch.js
@@ -3,33 +3,6 @@
 if(navigator.serviceWorker) {
 
   navigator.serviceWorker.register('/sw.js').then((registration) => {
-
-    if(registration.active) {
-      registration.active.postMessage('respond to this')
-    }
-
-
+    
   }).catch(console.log)
-
-  navigator.serviceWorker.addEventListener('message', (e) => {
-    console.log(e.data)
-  })
-
 }
-
-
-
-//fetch camera feed
-//
-// fetch('camera_feed.html').then((res) => {
-//   return res.text()
-// })
-//   .then((html) => {
-//     document.getElementById('camera').innerHTML = html
-//   })
-
-// fetch('/template.html').then((response) => {
-//   return response.text()
-// }).then((html) => {
-//   document.getElementById('body').innerHTML = html
-// })
