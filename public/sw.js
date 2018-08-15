c0a40a7d43fa3a06f7f035af720d5af735e377e5
James Andrews <bdaddyja@gmail.com>
2018-08-14 22:33:25 -0700

    remove message eventlistener from service worker.

--- public/sw.js
+++ public/sw.js
@@ -1,30 +1,9 @@
 
+self.addEventListener('push', (e) => {
 
-self.addEventListener('message', (e) => {
-  // if(e.request.url.endsWith('/greet')) {
-  //
-  //   let headers = new Headers({'Content-Type': 'text/html'})
-  //   let customRes = new Response('<h1>Hello World</h1>', {headers})
-  //
-  //   e.respondWith(customRes)
-  // }
-
-  self.clients.matchAll().then((clients) => {
-    clients.forEach((client) => {
-      client.postMessage("Hello from serviceWorker!")
-    })
-  })
+  let n = self.registration.showNotification('A notification from the SW')
 
 
-  // if(e.request.url.endsWith('/camera_feed.html')) {
-  //
-  //   e.respondWith(
-  //     fetch(e.request)
-  //       .then((res) => {
-  //         if(res.ok) return res
-  //
-  //         return new Response('Camera feed currently not avaiable.')
-  //       })
-  //   )
-  // }
+  e.waitUntil(n)
+
 })
