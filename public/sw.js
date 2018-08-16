ae36d4e611bc0e788da4ccb91dc23ba6fc043dd1
James Andrews <bdaddyja@gmail.com>
2018-08-15 20:20:38 -0700

    Register service worker cache.

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,31 @@
+//
+// self.addEventListener('push', (e) => {
+//
+//   let n = self.registration.showNotification('A notification from the SW')
+//
+//
+//   e.waitUntil(n)
+//
+// })
 
-self.addEventListener('push', (e) => {
+const pwaCache = 'pwa-cache-1'
 
-  let n = self.registration.showNotification('A notification from the SW')
+self.addEventListener('install', (e) => {
+  let cacheReady = caches.open(pwaCache).then((cache) => {
+    console.log('New Cache Ready')
 
+    return cache.add('/')
+  })
 
-  e.waitUntil(n)
+  e.waitUntil(cacheReady)
+})
 
+self.addEventListener('fetch', (e) => {
+  if(e.request.url === 'http://localhost:3000/') {
+    let newRes = caches.open(pwaCache).then((cache) => {
+      return cache.match(e.request)
+    })
+
+    e.respondWith(newRes)
+  }
 })
